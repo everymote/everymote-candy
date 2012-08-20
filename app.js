5268510bc8965a25c6338ad95d82efd8db405e33
Marcus <marcus@solidkod.nu>
2012-08-20 17:55:40 +0200

    stuff

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ var io = require('socket.io-client'),
    tellstick = require('./candy');
 
 var port = '1337',
-	server =  'm.everymote.com';
+	server =  '127.0.0.1';
 
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1338, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1338/');
 
 var connectCandy = function(thing){
 	console.log(thing);
-	var socket = io.connect('http://' + server + ':' + port + '/thing',{"force new connection":true});
+	var socket = io.connect('http://' + server + ':' + port + '/thing'
+		,{"force new connection":true
+		  ,'reconnect': true
+  		  ,'reconnection delay': 5000
+  		  ,'reconnection limit': 5000
+  		  ,'max reconnection attempts': 10 });
 	
 	socket.on('connect', function () {
 		console.log('connected');
@@ -26,6 +38,12 @@ var connectCandy = function(thing){
 		console.log('error:' + socket );
 	}).on('disconnect', function () {
 		console.log('disconnected');
+	}).on('reconnect', function () {
+		console.log('reconnect');
+	}).on('reconnecting', function () {
+		console.log('reconnecting');
+	}).on('reconnect_failed', function () {
+		console.log('reconnect_failed');
 	});
 };
 
